6689bb4a928797bb71a1cc1d7c9927391fa86a58
skydihy <guylaxy.dev@gmail.com>
2022-10-12 17:12:01 +0700

    Update: connect local db, display users

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 const express = require("express");
-
+const { createConnection } = require("mysql");
 const app = express();
 
+// setup - only development
+const connection = createConnection({
+  host: "localhost",
+  user: "root",
+  password: "12345600",
+  database: "join_us",
+});
+
 app.get("/", (req, res) => {
-    res.send('reached the homepage')
-  console.log("someone requested us");
+  const sqlQuery = "SELECT COUNT(*) AS count FROM users";
+
+  connection.query(sqlQuery, (err, response) => {
+    if (err) throw err;
+    console.log(response);
+    const userCount = response[0].count;
+    res.send(`We have ${userCount} users in our db`);
+  });
 });
 
 app.listen(8080, () => {
